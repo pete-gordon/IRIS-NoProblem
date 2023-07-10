4acd3207978d29db6f2afbb13624892b052e9183
Peter Gordon <pete@gordon.plus>
2023-07-10 21:35:20 +0100

    Tidyups and finishingsingings

--- runit.bat
+++ runit.bat
@@ -2,4 +2,4 @@ copy noproblm.tap C:\Stuff\Dev\Windows\Oricutron\svn\tapes
 copy noproblm.sym C:\Stuff\Dev\Windows\Oricutron\svn\tapes
 cd \Stuff\Dev\Windows\Oricutron\svn
 oricutron -t tapes/noproblm.tap -s tapes/noproblm.sym -r demostart
-cd \Stuff\Dev\Windows\oric_demo_for_nova
+cd \Stuff\Dev\Oric_Demos\NoProblem
--- runit_nobp.bat
+++ runit_nobp.bat
@@ -2,4 +2,5 @@ copy noproblm.tap C:\Stuff\Dev\Windows\Oricutron\svn\tapes
 copy noproblm.sym C:\Stuff\Dev\Windows\Oricutron\svn\tapes
 cd \Stuff\Dev\Windows\Oricutron\svn
 oricutron -t tapes/noproblm.tap -s tapes/noproblm.sym
-cd \Stuff\Dev\Windows\oric_demo_for_nova
+cd \Stuff\Dev\Oric_Demos\NoProblem
+
